8443fe47d6b33a3ad8eb7f565c2bb3a486e492d3
dmalikzadeh <71763474+dmalikzadeh@users.noreply.github.com>
2024-12-02 16:34:38 +0000

    Set up database connection and test query

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express'
 import pg from 'pg'
 
+const app = express();
+const { Pool } = pg
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+  });
+
+async function testConnection() {
+    try {
+        const res = await pool.query('SELECT * FROM students');
+        console.log('Database Test Result:', res.rows);
+    } catch (err) {
+        console.error(err);
+    }
+}
+  
+testConnection();
