fd3445cb96a1f2d1a95e0f9b93d06e19bdd6007f
replicant <vova15122004@mail.ru>
2024-12-08 20:00:17 +0000

    Update dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import basket from './pages/basket';
 import mainPage from './pages/mainPage';
