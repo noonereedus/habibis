6723d5cb614025ff870b11572a3aa3d209e97cb7
replicant <vova15122004@mail.ru>
2024-12-08 15:58:52 +0000

    Add page routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import basket from './pages/basket';
+import mainPage from './pages/mainPage';
+import shopping from './pages/shopping';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/basket">Basket</Link>
+        <Link to="/contact">Shop</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<mainPage />} />
+        <Route path="/basket" element={<basket />} />
+        <Route path="/shopping" element={<shopping />} />
+      </Routes>
+    </Router>
   );
 }
 
