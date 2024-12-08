daf11a224d300f5c1e5330845b6d588e01666cd7
replicant <vova15122004@mail.ru>
2024-12-08 21:03:31 +0000

    Add backend interaction

--- frontend/src/pages/SharedOrder.js
+++ frontend/src/pages/SharedOrder.js
@@ -2,8 +2,23 @@ import {useState, useEffect} from 'react'
 
 function SharedOrder() {
 
+    const [basket, setBasket] = useState({});
+    useEffect(() => {
+        fetch("http://localhost:3000/products")
+        .then(res => res.json())
+        .then(data => setData(products))
+    }, []
+)
+
 return (
-    <div><h1> Shared Order</h1></div>
+    <div><h1> Shared Order</h1>
+    <ul>
+        {products.map((product) => (
+            <li>{product.name}: {product.price}</li>
+        ))}
+    </ul>
+    </div>
+    
 );
 
 }
