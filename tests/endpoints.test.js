a436a85ef28f2b5ed61a4ecb00630c335d2c2561
Liba Mahmood <lxm613@student.bham.ac.uk>
2024-12-09 19:10:25 +0000

    Add test order and add multiple students to order dynamically

--- tests/endpoints.test.js
+++ tests/endpoints.test.js
@@ -3,6 +3,30 @@ import { app, server } from '../src/index.js';
 import pool from '../src/utils.js';
 
 describe("Endpoint Tests", () => {
+    let testOrderId;
+    let testStudentIds = [2644476, 2563027, 2545776, 2521768];
+    let testUniqueCode = 'TEST123';
+
+    beforeAll(async () => {
+        // create a test order 
+        const orderResult = await pool.query(
+            `INSERT INTO shared_orders (created_by, unique_code)
+             VALUES ($1, $2) RETURNING id, unique_code`
+            [testStudentIds[0], testUniqueCode]
+        );
+        testOrderId = orderResult.rows[0].id;
+
+        // add multiple students to the order
+        for (const studentId of testStudentIds) {
+            await pool.query(
+                `INSERT INTO student_contributions (order_id, student_id, delivery_fee_share)
+                 VALUES ($1, $2, $3)`,
+                [testOrderId, studentId, 5.00]
+            );
+        }
+
+    });
+
     // TODO: order management (create, code, status, join, remove)
     describe("Order management",() => {
         test("POST /order/create should create a new order", async () => {
