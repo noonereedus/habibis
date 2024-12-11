3aedeb463fcc7024efe7a38939ba256f365a23bf
replicant <vova15122004@mail.ru>
2024-12-11 00:45:11 +0000

    Make pop-up warning constant

--- frontend/src/pages/SharedOrder.js
+++ frontend/src/pages/SharedOrder.js
@@ -16,9 +16,9 @@ function SharedOrder() {
 return (
     <div><h1> Shared Order</h1>
     <ul>
-        {products.length == 0 && (
-            <><h2>The backend server is hosted locally.</h2><h3>If the space below is empty,
-                    please clone the repository, run "node src/index.js" in terminal and reload this page</h3></>)}
+       
+            <><h3>The backend server is hosted locally.</h3><h4>If the space below is empty,
+                    please clone the repository, run "node src/index.js" in terminal and reload this page</h4></>
         {products.map((product) => (
             <li>{product.name}: {product.price}</li>
         ))}
