ae1e5779de4ab1915da288bf065d79e7ad789e94
replicant <vova15122004@mail.ru>
2024-12-11 00:40:18 +0000

    Add pop-up instructions

--- frontend/src/pages/SharedOrder.js
+++ frontend/src/pages/SharedOrder.js
@@ -18,6 +18,9 @@ return (
     <h3>The backend server is hosted locally.</h3><h4>If the space below is empty,
     please clone the repository, run "node src/index.js" in terminal and reload this page</h4>
     <ul>
+        {products.length == 0 && (
+            <><h2>The backend server is hosted locally.</h2><h3>If the space below is empty,
+                    please clone the repository, run "node src/index.js" in terminal and reload this page</h3></>)}
         {products.map((product) => (
             <li>{product.name}: {product.price}</li>
         ))}
