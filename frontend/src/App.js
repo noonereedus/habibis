c0fb2863c1b109c19fdd2bcf2b1fab72bd314870
replicant <vova15122004@mail.ru>
2024-12-11 01:59:40 +0000

    Fix routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import './styles.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import SharedOrder from './pages/SharedOrder';
-// import mainPage from './pages/mainPage';
-// import shopping from './pages/shopping';
+import MainPage from './pages/mainPage';
+import Shopping from './pages/shopping';
 
 function App() {
   return (
     <Router>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/MainPage">Home</Link>
         <Link to="/SharedOrder">SharedOrder</Link>
-        <Link to="/shopping">Shop</Link>
+        <Link to="/Shopping">Shop</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<mainPage />} />
+        <Route path="/MainPage" element={<MainPage />} />
         <Route path="/SharedOrder" element={<SharedOrder />} />
-        <Route path="/shopping" element={<shopping />} />
+        <Route path="/Shopping" element={<shopping />} />
       </Routes>
     </Router>
   );
