bed31902d65dc213bb6ba5f1e4544654683b5ff1
Liba4M <libam1714@gmail.com>
2024-12-11 22:41:24 +0000

    Fix database reset error in reset script

--- src/reset-db.js
+++ src/reset-db.js
@@ -13,9 +13,10 @@ async function reset() {
     
         // reinsert initial hardcoded/seed data into shared_orders
         await pool.query(`
-          INSERT INTO shared_orders (created_by, unique_code) VALUES 
-          ('2644476', '5XYD3'),
-          ('2521768', 'BR3FQ');
+          ALTER SEQUENCE shared_orders_id_seq RESTART WITH 1;
+          INSERT INTO shared_orders (id, created_by, unique_code) VALUES 
+          (1, '2644476', '5XYD3'),
+          (2, '2521768', 'BR3FQ');
         `);
     
         // reinsert initial data into shared_order_items for order 1 - 5XYD3
@@ -25,13 +26,8 @@ async function reset() {
           (13, 1, '2545776', 1),
           (6, 1, '2563027', 3),
           (7, 1, '2563027', 3),
-          (17, 1, '2644476', 1);
-        `);
-    
-        // reinsert initial data into shared_order_items for shared order 2 - BR3FQ
-        await pool.query(`
-            INSERT INTO shared_order_items (item_id, order_id, student_id, quantity) VALUES
-            (20, 2, '2521768', 12);
+          (17, 1, '2644476', 1),
+          (20, 2, '2521768', 12);
         `);
 
         // reinsert initial data into student_contributions for shared order 1 - 5XYD3 
@@ -39,12 +35,7 @@ async function reset() {
             INSERT INTO student_contributions (order_id, student_id) VALUES
             (1, '2644476'),
             (1, '2545776'),
-            (1, '2563027');
-        `);
-
-        // reinsert initial data into student_contributions for shared order 2 - BR3FQ 
-        await pool.query(`
-            INSERT INTO student_contributions (order_id, student_id) VALUES
+            (1, '2563027'),
             (2, '2521768');
         `);
     
@@ -58,7 +49,6 @@ async function reset() {
         // if any query fails then roll back the transaction
         await pool.query('ROLLBACK');
         console.error('Error during database reset:', err);
-        process.exit(1); 
       }
 
 }
