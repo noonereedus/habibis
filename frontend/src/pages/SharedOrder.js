2dc74309196d353b906dd1f7b7ac98968e334a6c
replicant <vova15122004@mail.ru>
2024-12-11 00:40:18 +0000

    Add pop-up instructions

--- frontend/src/pages/SharedOrder.js
+++ frontend/src/pages/SharedOrder.js
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react'
 
+//Makes a backend request to access the grocery list
 function SharedOrder() {
 
     const [products, setProducts] = useState([]);
@@ -11,9 +12,13 @@ function SharedOrder() {
     }, []
 )
 
+//Produces HTML layout to display the website nodes
 return (
     <div><h1> Shared Order</h1>
     <ul>
+        {products.length == 0 && (
+            <><h2>The backend server is hosted locally.</h2><h3>If the space below is empty,
+                    please clone the repository, run "node src/index.js" in terminal and reload this page</h3></>)}
         {products.map((product) => (
             <li>{product.name}: {product.price}</li>
         ))}
