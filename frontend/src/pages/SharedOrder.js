ee70305e62659e80f952c8d95e9dea1d10cf203d
replicant <vova15122004@mail.ru>
2024-12-11 01:13:40 +0000

    Reverse previous commit

--- frontend/src/pages/SharedOrder.js
+++ frontend/src/pages/SharedOrder.js
@@ -15,7 +15,6 @@ function SharedOrder() {
 //Produces HTML layout to display the website nodes
 return (
     <div><h1> Shared Order</h1>
-    <h2>Does anything change when I commit?..</h2>
     <ul>
         {products.length === 0 && (
             <><h2>The backend server is hosted locally.</h2><h3>If the space below is empty,
