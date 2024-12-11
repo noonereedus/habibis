02be13bb9b8883c2c3c3dd58a35ecc72aae77ee5
replicant <vova15122004@mail.ru>
2024-12-11 00:55:23 +0000

    Revert pop-up visibility

--- frontend/src/pages/SharedOrder.js
+++ frontend/src/pages/SharedOrder.js
@@ -15,8 +15,6 @@ function SharedOrder() {
 //Produces HTML layout to display the website nodes
 return (
     <div><h1> Shared Order</h1>
-    <h3>The backend server is hosted locally.</h3><h4>If the space below is empty,
-    please clone the repository, run "node src/index.js" in terminal and reload this page</h4>
     <ul>
         {products.length == 0 && (
             <><h2>The backend server is hosted locally.</h2><h3>If the space below is empty,
